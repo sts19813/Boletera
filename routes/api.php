ca77f2712a8071636606899647b3e5380fc1fba9
sts19813 <sts19813@gmail.com>
2025-12-17 11:24:21 -0600

    cambio para que funcione en prod

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProjectController;
-use App\Http\Controllers\Api\PhaseController;
-use App\Http\Controllers\Api\StageController;
-use App\Http\Controllers\Api\LotController;
+use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\PhaseController;
+use App\Http\Controllers\API\StageController;
+use App\Http\Controllers\API\LotController;
 use App\Http\Controllers\View\TicketViewController;
 // Proyectos
 Route::apiResource('projects', ProjectController::class);
