567100c17fca76c95d55d2c1637d685a6952b4ac
sts19813 <sts19813@gmail.com>
2026-01-06 12:16:35 -0600

    google analitycs

--- resources/views/layouts/iframe.blade.php
+++ resources/views/layouts/iframe.blade.php
@@ -2,6 +2,15 @@
 <html lang="en">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPF7EK17R3"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-HPF7EK17R3');
+    </script>
     <!-- Google Tag Manager -->
     <script>(function (w, d, s, l, i) {
             w[l] = w[l] || []; w[l].push({
